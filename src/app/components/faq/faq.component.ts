ddd196398807b55bdbc1500689c1421b8993dee3
kwinkich <vankord21@gmail.com>
2024-09-04 22:21:21 +0300

    update title first screen & add faq content

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -11,17 +11,29 @@ import { FaqItemComponent } from './faq-item/faq-item.component';
 export class FaqComponent {
   faqItems = [
     {
-      title: 'Question 1',
+      title: 'What is Dywe?',
       content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquid quam molestias alias, corrupti debitis consectetur temporibus fugit earum placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquid quam molestias alias, corrupti debitis consectetur temporibus fugit earum placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquid quam molestias alias, corrupti debitis consectetur temporibus fugit earum placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aliquid quam molestias alias, corrupti debitis consectetur temporibus fugit earum placeat.',
+        'Dywe is the first decentralised exchange within TON, offering a wide range of trading options. NFT and token futures trading, copy trading, and demo trading are available on the platform.',
     },
-    { title: 'Question 2', content: 'Answer 2' },
-    { title: 'Question 3', content: 'Answer 2' },
-    { title: 'Question 4', content: 'Answer 2' },
-    { title: 'Question 5', content: 'Answer 2' },
     {
-      title: 'Very looooooong question and very looooooong answer',
-      content: 'Answer 2',
+      title: 'How does Dywe provide security to users?',
+      content:
+        'Dywe provides security through smart contracts that automate and protect all transactions on the platform. Regular audits and advanced security technologies help minimise risk and ensure transparency',
+    },
+    {
+      title: 'How is futures trading done at Dywe?',
+      content:
+        'Futures trading on Dywe allows you to make trades based on predictions of NFT and token price movements. You can use various trading strategies to capitalise on market fluctuations.',
+    },
+    {
+      title: 'How does copytrading on Dywe work?',
+      content:
+        'Copytrading on Dywe allows users to automatically replicate the trades of successful traders. You can choose a trader with a good history and follow their strategies to maximise your investment.',
+    },
+    {
+      title: 'What assets can be traded on Dywe?',
+      content:
+        'NFTs and tokens are available for trading on Dywe, providing a wide range of investment and diversification opportunities.',
     },
   ];
   activeIndex: number | null = null;
