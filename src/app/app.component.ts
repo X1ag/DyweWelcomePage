b536db51d94e7376aeec7b244b6cb4fc53c52931
kwinkich <vankord21@gmail.com>
2024-09-04 03:47:38 +0300

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaqComponent } from './components/faq/faq.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { MetricsComponent } from './components/metrics/metrics.component';
@@ -18,6 +19,7 @@ import { SocialComponent } from './components/social/social.component';
     MetricsComponent,
     SocialComponent,
     FaqComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
