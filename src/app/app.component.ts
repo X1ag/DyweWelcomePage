a21db26d770a04f027e920d278b8c553135aac04
kwinkich <vankord21@gmail.com>
2024-09-04 13:14:19 +0300

    added animation for faq block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FaqComponent } from './components/faq/faq.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
-import { MetricsComponent } from './components/metrics/metrics.component';
-import { NavMobileComponent } from './components/nav-mobile/nav-mobile.component';
-import { SocialComponent } from './components/social/social.component';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { FaqComponent } from './components/faq/faq.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { HeaderComponent } from './components/header/header.component'
+import { MainComponent } from './components/main/main.component'
+import { MetricsComponent } from './components/metrics/metrics.component'
+import { NavMobileComponent } from './components/nav-mobile/nav-mobile.component'
+import { SocialComponent } from './components/social/social.component'
 
 @Component({
   selector: 'app-root',
